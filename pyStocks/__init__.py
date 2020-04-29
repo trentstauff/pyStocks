9b690eaaafcb8a22d0c23e0dd7da194d38e75e52
Trent Stauffer <tstauffe@edu.uwaterloo.ca>
2020-04-28 21:46:49 -0400

    init.py fix

--- pyStocks/__init__.py
+++ pyStocks/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = os.environ.get('MY_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
